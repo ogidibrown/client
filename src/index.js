f66870b80c0741dc3eaf623ce5eece1a2093c75a
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-04-26 21:53:40 +0000

    secind

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+const CLIENT_ID = '4a749032c8d8e43a1e12'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <App />
-  </React.StrictMode>
+ 
 );
 
